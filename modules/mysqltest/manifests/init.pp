1143a7f293e9d9b60ffd96831d6800d01cca3a0c
mano <manoharendla277@gmail.com>
2018-06-07 12:08:07 +0530

    Update init.pp

--- modules/mysqltest/manifests/init.pp
+++ modules/mysqltest/manifests/init.pp
@@ -1,4 +1,30 @@
 class mysqltest {
-   
-
+  class { '::mysql::server':
+    root_password    => 'test_password',
+    override_options => {
+      'mysqld' => {
+        'max_connections' => '1024',
+       }
+    }
+    Service { 'mysqld':
+      ensure => running,
+    }
+    mysql::db { 'testdb':
+      user     => 'mano',
+      password => 'mano',
+      host     => 'server.mysql.com',
+    }
+    
+    mysql_user { 'mano@localhost':
+      ensure                   => 'present',
+      max_connections_per_hour => '60',
+    }
+    
+    mysql_grant { 'mano@localhost/testdb':
+      ensure => 'present',
+      options => ['GRANT'],
+      priviliges => ['ALL'],
+      table => 'newtable',
+      user  => 'mano@localhost',
+    }
 }
